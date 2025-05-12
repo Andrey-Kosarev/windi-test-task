681f2b24bf9ef9143bb6850a2ba8702a134111f9
Andrey Kosarev <example@email.com>
2025-05-12 20:10:52 +0300

    init

---
+++ src/core/models/chat.py
@@ -0,0 +1,15 @@
+from core.models.user import User
+from core.models.message import Message
+
+
+class Chat:
+    def __init__(self, name: str, admin: User, participants: list[User]):
+        self.name:str = name
+        self.participants: list[User] = participants
+        self.admins: list[User]
+
+    def send_message(self, message: Message):
+        ...
+
+    def add_user(self, user: User):
+        self.participants.app
---
+++ src/core/models/message.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timezone
+
+from core.models.user import User
+
+
+class Message:
+    def __init__(self, text: str, sender: User):
+        self.text: str = text
+        self.sender: User = sender
+        self.timestamp = datetime.now()
+        self.read_by: list[User] = []
+
+    
