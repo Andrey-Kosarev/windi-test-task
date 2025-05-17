a4eeb5c4d46d6ecb10946aa0b66462bcd6dc4dfe
Andrei Kosarev <kosarev_andrey@mail.ru>
2025-05-17 17:42:22 +0200

    fixed default timestamp of Message

--- src/domain/models/message.py
+++ src/domain/models/message.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 from src.domain.models.user import User
@@ -10,5 +10,5 @@ class Message:
     sender_id: int
     chat_id: int
     id: Optional[int] = None
-    timestamp: datetime = datetime.now()
+    timestamp: datetime =  field(default_factory=datetime.now)
     is_read: bool = False
